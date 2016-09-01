8ba2a8664a397f97a4e26197d25dbe3d727dafc9
Alexanderlau <429095816@qq.com>
2016-09-01 22:13:50 +0800

    2016

---
+++ 048.py
@@ -0,0 +1,23 @@
+from zipfile import ZipFile
+from urllib.request import urlopen
+from io import BytesIO
+from bs4 import BeautifulSoup
+
+wordFile = urlopen("http://pythonscraping.com/pages/AWordDocument.docx").read()
+wordFile = BytesIO(wordFile)
+document = ZipFile(wordFile)
+xml_content = document.read('word/document.xml')
+
+wordObj = BeautifulSoup(xml_content.decode('utf-8'))
+textString = wordObj.findAll("w:t")
+for textElem in textString:
+    clostTag = ""
+    try:
+        style = textElem.parent.previousSibling.find("wLpstyle")
+        if style is not None and style["w:val"] == "Title":
+         print("<ch1>")
+         clostTag = "</h1>"
+    except AttributeError:
+        pass
+        print(textElem.text)
+        print(clostTag)
